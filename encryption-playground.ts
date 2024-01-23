406330524c7be262e0f485da07c09675a145c441
Jensen Omega <omega90ej@gmail.com>
2024-01-22 21:23:13 -0800

    started bcrypt lesson

---
+++ encryption-playground.ts
@@ -0,0 +1,18 @@
+import bcrypt from 'bcrypt';
+
+const password = 'password';
+
+
+console.log('starting')
+bcrypt.hash(password, 11).then((result) => {
+   console.log('done');
+   console.log({ result })
+})
+
+
+bcrypt.compare(
+   password,
+   '$2b$11$CiTD0wObpDUH2MwwDdo0je3gsftzRGv.xE7X5g/2VG.saqB7.rB66'
+).then((result) => {
+   console.log({result: result})
+})
